68eda16e1c23c34699ca2604e41b386065f421c9
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2026-08-26 16:26:12 +0900

    fcitx5

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -142,9 +142,28 @@ in
     wrapperFeatures.gtk = true;
   };
 
-  i18n.inputMethod.type = "ibus";
-  i18n.inputMethod.ibus.waylandFrontend = true;
-  i18n.inputMethod.ibus.engines = [ "mozc" ];
+  i18n.inputMethod.enable = true;
+  i18n.inputMethod.type = "fcitx5";
+  i18n.inputMethod.fcitx5.waylandFrontend = true;
+  i18n.inputMethod.fcitx5.addons = [ pkgs.fcitx5-mozc ];
+  i18n.inputMethod.fcitx5.settings.inputMethod = {
+    GroupOrder = {
+      "0" = "Default";
+    };
+    "Groups/0" = {
+      Name = "Default";
+      "Default Layout" = "us";
+      DefaultIM = "mozc";
+    };
+    "Groups/0/Items/0" = {
+      Name = "keyboard-us";
+      Layout = "";
+    };
+    "Groups/0/Items/1" = {
+      Name = "mozc";
+      Layout = "";
+    };
+  };
 
   sops = {
   	age.keyFile = ageKeyFile;
